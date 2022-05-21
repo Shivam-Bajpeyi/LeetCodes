a1dd310025435f6b41cb5f7972132aa75faa0fb3
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-05-22 03:28:51 +0530

    Time: 160 ms (27.61%), Space: 49.1 MB (13.45%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    //It is just like Unbounded knapsack problem
-    int coinChange(vector<int>& coins, int amount) 
-    {
-        //define no. of coins and dp matrix of rows=no. of coins and column= total amount 
-        int n = coins.size(); 
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, 1e5)); 
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 1e5));
         
-        //i represent coin value
+        //i: represents coin (coin[i-1])
         for(int i=0; i<=n; i++)
         {
-            //j represent amount to be reach
+            //j: represents amount
             for(int j=0; j<=amount; j++)
             {
-                //if amount value is 0 then we need 0 coins 
-                if(j==0) 
+                if(j==0)    //to return 0 amount, we dont need any coin
                     dp[i][j] = 0;
                 
-                //if we have 0 value coin then we need infinite coins to reach amount
-                else if(i==0)  
+                else if(i==0)    //0 coin can be given infinity times to return an anount
                     dp[i][j] = 1e5;
                 
-                //if coin value is greater than amount needed then we use exclusion case
-                //exclusion case - go one level up and copy paste the solution
-                else if(coins[i-1]>j) 
+                else if(coins[i-1] >j)   //coin value > amount (2> 1): then give coins of previous value coin                      
                     dp[i][j] = dp[i-1][j];
                 
-                //if we are able to take current coin then we find mini. of inclusion or exclusion case
-                //inclusion case - we add 1 to the dp since we use the coin and in the same level we find other coin (j-coins[i-1]) which can cover the remaining amount
-                else
+                else    //amount > coin value (10 > 2): then give 1 coin of this value and also coins for dp[i][restAmount]
                     dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
             }
         }
-        return dp[n][amount]>1e4 ? -1 : dp[n][amount];           
+        
+        return dp[n][amount]>1e4 ? -1: dp[n][amount];
     }
 };
