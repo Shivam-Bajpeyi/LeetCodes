8be7004fcdd7a7d6580591b518eb7ea677a5185d
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-04 01:15:13 +0530

    Time: 7 ms (66.71%), Space: 10.8 MB (8.59%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -4,22 +4,18 @@ public:
         vector<int> ans;
         unordered_map<int, int> mpp;
         
-        //count frequency of nums2 elements
-        for(int i=0; i<nums2.size(); i++)
-            mpp[nums2[i]]++;
+        //count frequency of nums1 elements
+        for(int i=0; i<nums1.size(); i++)
+            mpp[nums1[i]]++;
         
-        //take element one by one from nums1 and start matching to nums2's elements
-        //if matched=> nums1[i] == nums2[j] then we will decrease its frequency by 1
-        for(int i=0; i<nums1.size(); i++){
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i]==nums2[j] && mpp[nums2[j]]!= 0){    //element's frequency should not be 0
-                    ans.push_back(nums1[i]);
-                    mpp[nums2[j]]--;    //decrease frequency
-                    break;
-                }
-                    
+        //find if nums2 element present in nums1 using map
+        //if matched=> then we will decrease its frequency by 1
+            for(int i=0; i<nums2.size(); i++){
+                if(mpp[nums2[i]] > 0){    //element's frequency should not be 0
+                    ans.push_back(nums2[i]);
+                    mpp[nums2[i]]--;    //decrease frequency
+                }     
             }
-        }
         
         return ans;
     }
