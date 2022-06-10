050a399b8a58119949277b00a823bf8902cee5d9
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-11 01:20:00 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.01%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    //tabulation(bottom-up approach)
+    //tabulation(bottom-up approach) with optimized space complexity
     int fib(int n) {
         if(n<=1)
             return n;
         
-        vector<int> dp(n+1, -1);
-        dp[0] = 0; dp[1] = 1;   //initialize base cases
-        
+        int prev2 = 0;
+        int prev1 = 1;
+    
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
         }
         
-        return dp[n];
+        return prev1;
     }
 };
