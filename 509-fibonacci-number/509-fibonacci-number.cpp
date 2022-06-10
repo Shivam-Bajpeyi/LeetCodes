244345c83c7d69a7af147c63693eff213f5106b2
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-11 01:12:54 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    //memoization
+    //tabulation(bottom-up approach)
     int fib(int n) {
-        vector<int> dp(n+1, -1);
-        return f(dp, n);
-    }
-    
-    int f(vector<int> &dp, int n){
         if(n<=1)
             return n;
-        if(dp[n] != -1) return dp[n];   //subproblem solved previously
-        return dp[n] = f(dp, n-1) + f(dp, n-2); //store ans in dp[]
+        
+        vector<int> dp(n+1, -1);
+        dp[0] = 0; dp[1] = 1;   //initialize base cases
+        
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
 };
