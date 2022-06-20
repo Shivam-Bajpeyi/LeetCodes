99992cfc071a19bd4f3d49174ad179d4d45f87af
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-20 23:41:09 +0530

    Time: 19 ms (25.46%), Space: 10 MB (58.93%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,37 +1,40 @@
-//DP_tabulation (bottom- up approach)
+//DP_tabulation (space- optimized)
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1)); //step1
+        //store previous row to take care of dp[i-1][j] element
+        vector<int> prev(m, -1); 
         
         //build dp from values calculated initially
         for(int i=0; i<n; i++){
+            vector<int> curr(m, -1);   //stores current row
             for(int j=0; j<m; j++){
-                if(i==0 && j==0) dp[i][j]= grid[0][0];
+                if(i==0 && j==0) curr[j] = grid[i][j];
                 
                 else{
                     int up = grid[i][j];            //go up, consider current value in sum
                     if(i>0)  
-                        up+= dp[i-1][j];       //base cases
+                        up+= prev[j];       //add up previous row value
                     else 
                         up+= 1e6;
                     
                     int left = grid[i][j];         //go left, consider current value in sum
                     if(j>0) 
-                        left += dp[i][j-1];   //base case
+                        left += curr[j-1];   //base case
                     else
                         left += 1e6;
                     
-                    cout<<min(up, left)<<endl;
-                    dp[i][j] = min(up, left);
+                    // cout<<min(up, left)<<endl;
+                    curr[j] = min(up, left);
                 
                 }
             }
+            prev = curr;
         }
         
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
