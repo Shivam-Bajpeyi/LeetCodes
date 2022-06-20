e640aa90e4619e4b294afa727e19bda9e6ca3b54
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-20 23:00:33 +0530

    Time: 25 ms (9.08%), Space: 10.2 MB (6.07%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,27 @@
+//DP- memoization
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1)); //step1
+        return f(n-1, m-1, grid, dp);
+    }
+    
+    int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp){
+        if(i==0 && j==0)    //return value if reached to goal 
+            return grid[i][j];
+        
+        if(i<0 || j<0)  //don't consider this path so return a max value
+            return 1e6;
+        
+        if(dp[i][j] != -1) return dp[i][j]; //step2
+        
+        int up   =  grid[i][j] + f(i-1, j, grid, dp);
+        int left =  grid[i][j] + f(i, j-1, grid, dp);
+        
+        return dp[i][j] = min(up, left);   //take min sum(step3)
+    }
+};
