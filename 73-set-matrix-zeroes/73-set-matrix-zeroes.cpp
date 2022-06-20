a8f24761efd08624841ed5778a1e7a937fa15536
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-20 11:02:23 +0530

    Time: 31 ms (16.35%), Space: 13.2 MB (53.19%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,29 +1,29 @@
+//TC = O(N*M)
+//SC = O(M) + O(N)
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         
-        //step1- wherever we find 0, convert all its row and col elements to 'A' (except 0)
+        //make row & col vector to keep track which row and col has 0 element
+        vector<int> row(m, -1);
+        vector<int> col(n, -1);
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    for(int k=0; k<m; k++)  //make row A
-                        if(matrix[k][j] != 0)
-                            matrix[k][j] = 'A';
-                    
-                    for(int k=0; k<n; k++)  //make col A
-                        if(matrix[i][k] != 0)
-                            matrix[i][k] = 'A';
+                if(matrix[i][j] ==0){   //mark ith row and jth col = 0
+                    row[i] = 0;
+                    col[j] = 0;
                 }
-                    
             }
         }
         
-        //step2- convert back A to 0
+        //since we know that which row or col has 0 then its time to go throgh all the elements and if element belongs to that row/ col which have 0 make that element 0
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(matrix[i][j]== 'A')
-                    matrix[i][j] = 0;
+                if(row[i]==0 || col[j]==0)
+                    matrix[i][j]= 0;
             }
         }
     }
