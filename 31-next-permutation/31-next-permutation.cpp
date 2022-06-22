2ec1fd166177c517876a93859eb02a49f58e9dcc
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-22 12:58:51 +0530

    Time: 7 ms (55.38%), Space: 12 MB (93.09%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        //step-1 find ind1 from backward such that nums[i] < nums[i+1]
+        int ind1 = 0;
+        for(ind1=n-2; ind1>=0; ind1--)
+            if(nums[ind1] < nums[ind1+1])
+                break;
+        
+        //last permutation is given (3 2 1) return (1 2 3)
+        if(ind1< 0)
+            reverse(nums.begin(), nums.end());
+        
+        else{
+            //step-2 find ind2 from end till ind1 such that nums[i] > nums[ind1] 
+        int ind2 = 0;
+        for(int i=n-1; i>=ind1; i--){
+            if(nums[i] > nums[ind1]){
+                ind2= i;
+                break;
+            }
+                
+        }
+        
+        //step-3 swap (ind1, ind2) & reverse array from ind1+1 to end
+        swap(nums[ind1], nums[ind2]);
+        reverse(nums.begin()+ind1+1, nums.end());
+        }
+        
     }
 };
