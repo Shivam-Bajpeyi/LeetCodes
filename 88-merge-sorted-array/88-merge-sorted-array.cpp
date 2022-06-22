998f041f7b65951ef383327ad2a0a848925e6d22
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-23 01:31:42 +0530

    Time: 12 ms (7.77%), Space: 9.2 MB (29.12%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1, j = n-1;
+        int i= m-1, j = n-1;
         int k = m+n-1;
         
-        //merge sort, compare two elments at nums1[i] and nums2[j] whichever will be bigger will store that at k because we want to sort array in non- decreasing order in nums1
+        //fill large elements from the back
         while(i>=0 && j>=0){
             if(nums1[i] >= nums2[j])
                 nums1[k--] = nums1[i--];
@@ -12,11 +12,7 @@ public:
                 nums1[k--] = nums2[j--];
         }
         
-        //if nums1 has elements remaining
-        while(i>=0)
-            nums1[k--] = nums1[i--];
-        
-        //if nums2 has elements remaining
+        //if nums2 elements remains to fill
         while(j>=0)
             nums1[k--] = nums2[j--];
     }
