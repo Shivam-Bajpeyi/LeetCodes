18aabf9a64020e3114c2dbc65bdde164b4e00ab6
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-22 14:25:11 +0530

    Time: 175 ms (57.72%), Space: 93.3 MB (52.10%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int currentPrice = prices[0];
         int profit = 0;
+        int buy = prices[0];
         
         for(int i=1; i<prices.size(); i++){
-            if(prices[i] < currentPrice)       //if stock price gets down, we will start purchasing that day so that we can book profit when price will be high
-                currentPrice = prices[i];
-            else{    //if stock price gets down, its time to calculate profit, if we are getting max profit then sell it o/w not
-                if(prices[i]-currentPrice > profit)
-                    profit = prices[i] - currentPrice;
-            }
+            if(prices[i] > buy) //book profit when price gets higher
+                profit = max(profit, prices[i]-buy);
+            else    //buy when price gets dowb
+                buy = prices[i];
         }
         
         return profit;
