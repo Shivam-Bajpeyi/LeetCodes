625c0349104489f2ed563d762ea542f140802a6b
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-22 14:18:17 +0530

    Time: 4 ms (43.51%), Space: 8.3 MB (70.28%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,28 +1,22 @@
+//dutch national flag algorithm
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int ind = 0;    //used to index elements for sorting
-        int n = nums.size();
+        int low = 0, mid= 0, high = nums.size()-1;
         
-        vector<int> ans(n);
-        //fix 0s from the 0th index
-        for(int i=0; i<n; i++){
-            if(nums[i] == 0)
-                ans[ind++] = nums[i];
+        while(mid <= high){
+            if(nums[mid] ==0){
+                swap(nums[mid], nums[low]);
+                mid++; low++;
+            }
+            
+            else if(nums[mid]==1)
+                mid++;
+            
+            else{
+                swap(nums[high], nums[mid]);
+                high--;
+            }
         }
-        
-        //1s
-        for(int i=0; i<n; i++){
-            if(nums[i] == 1)
-                ans[ind++] = nums[i];
-        }
-        
-        //2s
-        for(int i=0; i<n; i++){
-            if(nums[i] == 2)
-                ans[ind++] = nums[i];
-        }
-        
-        nums = ans;
     }
 };
