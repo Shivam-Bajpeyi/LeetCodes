c0ef84dc93a424174b346d08533d121a76a82bea
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-06-26 17:34:53 +0530

    Time: 3 ms (43.35%), Space: 6.6 MB (25.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-  int f(int i, int j, vector<vector<int>> &dp){
-    if(i==0 && j==0) return 1;
-    if(i<0 || j<0) return 0;
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return f(m-1, n-1, dp);
+    }
     
-    if(dp[i][j] != -1) return dp[i][j];	//return if we have ans
-    int left = f(i, j-1, dp);
-    int up = f(i-1, j, dp);
-    
-    dp[i][j] = left+up;	//calc and store in dp
-    return dp[i][j];
-}
-
-int uniquePaths(int m, int n) {
-    vector<vector<int>> dp(m, vector<int>(n, -1)); //step1 - declare dp[m][n]
-    return f(m-1, n-1, dp); 
+    int f(int i, int j,  vector<vector<int>> &dp){
+        //out of the boundary
+        if(i<0 || j<0) 
+            return 0;   //0-> this is not a path
+        
+        if(i==0 && j==0)
+            return 1;   //got 1 path
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        //do all possible moves
+        int left = f(i, j-1, dp);
+        int up   = f(i-1, j, dp);
+        
+        //all ways= sum
+        return dp[i][j] = left + up;
     }
 };
