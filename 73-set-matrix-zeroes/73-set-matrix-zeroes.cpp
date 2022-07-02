e7868ba4a717a68e517b47816cccd672dbce7931
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-02 22:40:52 +0530

    Time: 34 ms (12.27%), Space: 13.2 MB (53.05%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,30 +1,35 @@
 //TC = O(N*M)
-//SC = O(M) + O(N)
+//SC = O(1)
 
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         
-        //make row & col vector to keep track which row and col has 0 element
-        vector<int> row(m, -1);
-        vector<int> col(n, -1);
+    //use matrix first row as row[] & first col as col[] and since matrix[0][0] is overlapping so 
+    //we wil use a variable col0 to check zero in 1st col and matrix[0][0] will be used to check 1st row
+        int col0 = 1;
         
+        //mark ith row and jth col as 0 if found 0 at matrix[i][j] place
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+            if(matrix[i][0] ==0) col0 = 0; //col0 has 0 value
+            
+            for(int j=1; j<n; j++){
                 if(matrix[i][j] ==0){   //mark ith row and jth col = 0
-                    row[i] = 0;
-                    col[j] = 0;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
         //since we know that which row or col has 0 then its time to go throgh all the elements and if element belongs to that row/ col which have 0 make that element 0
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(row[i]==0 || col[j]==0)
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=1; j--){
+                if(matrix[i][0] ==0 || matrix[0][j]==0)
                     matrix[i][j]= 0;
             }
+            if(col0==0)  //make 1st col 0 if col0 contains 0
+                matrix[i][0] = 0;
         }
     }
 };
