85262bf5d9308efec1237e25f742b534b77d3163
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-04 17:53:15 +0530

    Time: 7 ms (86.54%), Space: 15 MB (7.95%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,42 +11,40 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        //merge sort solution
-        vector<int> nums;
+        //merge sort
+        //make a new linkedlist with merged nodes(sorted)
+        ListNode* head = new ListNode(0, NULL);
+        ListNode* curr = head;  //used for forming LL
         
-        //step-1: merge LL1 & LL2 and store them in nums using merge sort
-        while(list1 != NULL && list2 != NULL){
-            if(list1->val <= list2->val){   //whichever node is smaller pick that
-                nums.push_back(list1->val);
-                list1 = list1->next;    //move pointer to the next node
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                curr->next = new ListNode(list1->val);
+                curr = curr->next;
+                list1 = list1->next;
             }
-            
             else{
-                nums.push_back(list2->val);
+                curr->next = new ListNode(list2->val);
+                curr = curr->next;
                 list2 = list2->next;
-            }   
+            }
         }
         
-        //if any of LLs' nodes are remaining, store them too
-        while(list1 != NULL){   //if LL1 nodes left
-            nums.push_back(list1->val);
+        while(list1){
+            curr->next = new ListNode(list1->val);
+            curr = curr->next;
             list1 = list1->next;
         }
         
-        while(list2 != NULL){
-            nums.push_back(list2->val);
+        while(list2){
+            curr->next = new ListNode(list2->val);
+            curr = curr->next;
             list2 = list2->next;
         }
         
-        //step-2: make a new LL of this ans vector to return as answer
-        ListNode* ans = new ListNode(NULL);
-        ListNode* cur = ans;    //cur will be used to make the LL
-        for(int i=0; i<nums.size(); i++){
-            cur->next = new ListNode(nums[i]);
-            cur = cur->next;
-        }
+        //add NULL node in the last
+        curr->next = NULL;
         
-        //step-3: We have made ans NULL and its next node is pointing to the first node of our ans LL, that's why ans->next
-        return ans->next;
+        //head= NULL -> [1-> 2-> 3-> NULL]
+        return head->next;  //1
     }
 };
