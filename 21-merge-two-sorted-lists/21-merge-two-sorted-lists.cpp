7e9cbb84a5183927d3c9ed48ca57d30181418635
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-04 18:16:35 +0530

    Time: 20 ms (12.69%), Space: 14.9 MB (42.45%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,40 +11,43 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        //merge sort
-        //make a new linkedlist with merged nodes(sorted)
-        ListNode* head = new ListNode(0, NULL);
-        ListNode* curr = head;  //used for forming LL
+        if(!list1)
+            return list2;
+        if(!list2)
+            return list1;
         
+        //In- place
+        //decide starting node: whiever is lesser
+        ListNode* curr = new ListNode(0, NULL);
+        
+        if(list1->val <= list2->val){
+            curr = list1;
+            list1 = list1->next;
+        }
+        else{
+            curr = list2;
+            list2 = list2->next;
+        }
+        ListNode* head = curr;  //to return head
+        
+        //start merging (merge sort)
         while(list1 && list2){
             if(list1->val <= list2->val){
-                curr->next = new ListNode(list1->val);
-                curr = curr->next;
+                curr->next = list1;
                 list1 = list1->next;
+                curr = curr->next;
             }
             else{
-                curr->next = new ListNode(list2->val);
+                curr->next = list2;
                 curr = curr->next;
                 list2 = list2->next;
             }
         }
+        if(list1)
+            curr->next = list1;
+        else
+            curr->next = list2;
         
-        while(list1){
-            curr->next = new ListNode(list1->val);
-            curr = curr->next;
-            list1 = list1->next;
-        }
-        
-        while(list2){
-            curr->next = new ListNode(list2->val);
-            curr = curr->next;
-            list2 = list2->next;
-        }
-        
-        //add NULL node in the last
-        curr->next = NULL;
-        
-        //head= NULL -> [1-> 2-> 3-> NULL]
-        return head->next;  //1
+        return head;
     }
 };
