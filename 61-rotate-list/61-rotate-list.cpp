dc7ccb8fba5a40bce618dfdfaca398ef307105fc
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-10 12:21:35 +0530

    Time: 8 ms (76.68%), Space: 11.8 MB (5.78%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,44 +10,38 @@
  */
 class Solution {
 public:
-    int totalNodes(ListNode* head){
-        int cnt = 0;
-        while(head){
-            cnt++;
-            head = head->next;
-        }
-        
-        return cnt;
-    }
-    
     ListNode* rotateRight(ListNode* head, int k) {
         if(head==NULL)
             return head;
         
-        //step-1: get actual rotation 
-        int n = totalNodes(head);
+        //step-1: get actual rotation
+        ListNode* curr = head;
+        ListNode* prev = new ListNode();
+        int n = 0;
+        while(curr){
+            n++;
+            prev = curr;
+            curr = curr->next;
+        }
+        
         k = k%n;    //4%3 = 1 rotation we need
         if(k==0)
             return head;
         
-        //step-2: point slow to the last node of rotated list
-        ListNode* slow = head;
-        ListNode* fast = head;
+        //step-1: hook last node of given LL to the head
+        prev->next = head;
+        
+        //step-2: get new head decide last node for the new LL
+        ListNode* newH = new ListNode();
+        k = n-k-1;  //traverse k times to reach last node
         while(k){
-            fast = fast->next;
+            head = head->next;
             k--;
         }
         
-        while(fast->next){
-            fast = fast->next;
-            slow = slow->next;
-        }
+        newH = head->next;  //new head
+        head->next = NULL;  //last node
         
-        //get head of rotated LL
-        ListNode* ans = slow->next;
-        slow->next = NULL;  //last node
-        fast->next = head;  //hook last node to given LL head
-        
-        return ans;
+        return newH;
     }
 };
