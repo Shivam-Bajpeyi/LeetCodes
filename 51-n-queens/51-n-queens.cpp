7f0a8c9186b49a72a3d5f19b09fb69b47f1f9a9b
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-17 23:24:31 +0530

    Time: 7 ms (78.21%), Space: 7.3 MB (70.91%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,64 +1,39 @@
 class Solution {
 private:
-    bool isPossible(int row, int col, vector<string> &board){
-        int duprow = row;
-        int dupcol = col;
-        //check upper left diag
-        while(duprow>=0 && dupcol>=0){
-            if(board[duprow][dupcol]== 'Q')
-                return false;
-            duprow--;
-            dupcol--;
-        }
-        
-        //left
-        duprow = row;
-        dupcol = col;
-        while(dupcol>=0){
-            if(board[duprow][dupcol]== 'Q')
-                return false;
-            dupcol--;
-        }
-        
-        //lower left diag
-        duprow = row;
-        dupcol = col;
-        while(duprow<board.size() && dupcol>=0){
-            if(board[duprow][dupcol]== 'Q')
-                return false;
-            duprow++;
-            dupcol--;
-        }
-        
-        return true;
-    }
-    
-    void solve(int col, vector<string> &board, vector<vector<string>> &ans){
+    void solve(int col, vector<string> &board,  vector<int> &leftRow, vector<int> &lowerDiag, vector<int> &upperDiag, vector<vector<string>> &ans){
         if(col== board.size()){
             ans.push_back(board);
             return;
         }
+    
         
         for(int row=0; row<board.size(); row++){
-            if(isPossible(row, col, board)){
-                board[row][col] = 'Q';
-                solve(col+1, board, ans);
+            if(leftRow[row]==0 && lowerDiag[row+col]==0 && upperDiag[board.size()-1 + col-  row]==0){    //is it possible to place queen here
+                board[row][col] = 'Q';  
+                leftRow[row] = 1;
+                lowerDiag[row+col] = 1;
+                upperDiag[board.size()-1+ col- row] = 1;
+                solve(col+1, board, leftRow, lowerDiag, upperDiag, ans);   //time to place next queen in the next col
                 board[row][col] = '.';
-            }
+                leftRow[row] = 0;
+                lowerDiag[row+col] = 0;
+                upperDiag[board.size()-1+ col- row] = 0;
+            } 
         }
-    }
     
+    }
+
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
         
         string str(n, '.');
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++)
             board[i] = str;
-        }
         
-        solve(0, board, ans);
+        vector<int> leftRow(n, 0), lowerDiag(2*n-1, 0), upperDiag(2*n-1, 0);
+        solve(0, board, leftRow, lowerDiag, upperDiag, ans);
         return ans;
     }
 };
