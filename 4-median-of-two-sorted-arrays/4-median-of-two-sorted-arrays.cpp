eb16411561337302b2996c69afd601335ba454c5
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-20 18:43:24 +0530

    Time: 43 ms (87.08%), Space: 89.8 MB (30.80%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,32 @@
+//Brute: merge sort (tc: N(log MN), sc: O(M+N))
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)   {
-        if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2, nums1);
-        
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        //merge arrays
         int n1 = nums1.size();
         int n2 = nums2.size();
+        vector<int> nums(n1+n2);    //to store merged array
         
-        int low = 0, high = n1;
-        while(low<=high){
-            int cut1 = (low + high)>>1;
-            int cut2 = (n1+n2+1)/2 - cut1;
-            
-            int l1 = (cut1==0) ? INT_MIN : nums1[cut1-1];
-            int l2 = (cut2==0) ? INT_MIN : nums2[cut2-1];
-            
-            int r1 = (cut1==n1) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2==n2) ? INT_MAX : nums2[cut2];
-            
-            if(l1<=r2 && l2<=r1){   //we did the right cut
-                if((n1+n2) %2 ==0)    //even length=> max(l1, l2)- min(r1, r2)
-                    return  (max(l1, l2)+ min(r1, r2))/ 2.0;
-                else    //odd length
-                    return max(l1, l2);
-            }
-            
-            else if(l1 > r2)
-                high = cut1-1;
+        //merge arrays
+        int i= 0, j= 0, k= 0;
+        while(i<n1 && j<n2){
+            if(nums1[i]<= nums2[j])
+                nums[k++] = nums1[i++];
             else
-                low = cut1+1;
+                nums[k++] = nums2[j++];
         }
         
-        return 0.0;
+        while(i<n1)
+            nums[k++] = nums1[i++];
+        while(j<n2)
+            nums[k++] = nums2[j++];
+        
+        //find median now in this sorted array
+        int n = n1+n2;
+        if(n%2)   //odd
+            return nums[n/2];
+        else
+            return (nums[n/2-1]+ nums[n/2])/2.0;
+            
     }
 };
