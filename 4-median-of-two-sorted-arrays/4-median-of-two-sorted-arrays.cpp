fd0b806841fdc80f1e90751a25845c585b10237b
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-20 19:25:31 +0530

    Time: 51 ms (72.67%), Space: 89.1 MB (96.97%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,58 @@
-//Brute: merge sort (tc: O(M+N), sc: O(M+N))
+//Brute: get indices of median elements using merge sort (tc: O(M+N), sc: O(1))
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        //merge arrays
         int n1 = nums1.size();
         int n2 = nums2.size();
-        vector<int> nums(n1+n2);    //to store merged array
         
-        //merge arrays
+        //just get the indices while merging not literally store all elements
+        int m1= (n1+n2)/2;
+        int m1Element = 0;
+        int m2 = (n1+n2)/2-1;
+        int m2Element = 0;
+        
         int i= 0, j= 0, k= 0;
         while(i<n1 && j<n2){
-            if(nums1[i]<= nums2[j])
-                nums[k++] = nums1[i++];
-            else
-                nums[k++] = nums2[j++];
+            if(nums1[i]<= nums2[j]){
+                if(m1==k)
+                    m1Element = nums1[i];
+                if(m2==k)
+                    m2Element = nums1[i];
+                ++k;  ++i;
+                
+            }
+                
+            else{
+                if(m1==k)
+                    m1Element = nums2[j];
+                if(m2==k)
+                    m2Element = nums2[j];
+                ++k; ++j; 
+            }
+                
         }
         
-        while(i<n1)
-            nums[k++] = nums1[i++];
-        while(j<n2)
-            nums[k++] = nums2[j++];
+        while(i<n1){
+            if(m1==k)
+                m1Element = nums1[i];
+            if(m2==k)
+                m2Element = nums1[i];
+            ++k;  ++i;
+        }
+        while(j<n2){
+            if(m1==k)
+                m1Element = nums2[j];
+            if(m2==k)
+                m2Element = nums2[j];
+            ++k; ++j;
+        }
         
         //find median now in this sorted array
         int n = n1+n2;
         if(n%2)   //odd
-            return nums[n/2];
+            return m1Element;
         else
-            return (nums[n/2-1]+ nums[n/2])/2.0;
+            return (m1Element + m2Element)/2.0;
             
     }
 };
