c65f1c8d63936dad0acf1b8deedb1b5de04f22bb
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-07-26 16:47:23 +0530

    Time: 6 ms (10.94%), Space: 6.9 MB (57.28%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,40 +1,34 @@
 class MyStack {
 public:
-    queue<int> q1;
-    queue<int> q2;
+    queue<int> que;
     MyStack() {
         
     }
     
     void push(int x) {
-        //q1->q2
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
+        int n = que.size();
+        que.push(x);
+        
+        //pop n elements and push it again to keep x at front
+        while(n--){
+            int temp = que.front(); que.pop();
+            que.push(temp);
         }
         
-        //x-> q1
-        q1.push(x);
-        
-        //q2-> q1
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
     }
     
     int pop() {
-        int ans = q1.front();
-        q1.pop();
+        int ans = que.front();
+        que.pop();
         return ans;
     }
     
     int top() {
-        return q1.front();
+        return que.front();
     }
     
     bool empty() {
-        return q1.size()==0;
+        return que.size()==0;
     }
 };
 
