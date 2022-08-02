405cd1af353efc07317be23505b39c94708bf7f2
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-02 23:13:52 +0530

    Time: 52 ms (5.17%), Space: 8.1 MB (30.55%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -8,33 +8,16 @@ public:
         mpp['I'] = 1;   mpp['V'] = 5;   mpp['X'] = 10;
         mpp['L'] = 50;  mpp['C'] = 100; mpp['D'] = 500; mpp['M'] = 1000;
         
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            if(i<n-1 && s[i]== 'I' && (s[i+1]=='V' || s[i+1]=='X')){
-                if(s[i+1]== 'V') ans = ans + mpp['V']- mpp['I'];
-                else if(s[i+1]== 'X') ans = ans + mpp['X']- mpp['I'];
-                i+= 1;
-            }
-            
-            else if(i<n-1 && s[i]== 'X' && (s[i+1]=='L' || s[i+1]=='C')){
-                if(s[i+1]== 'L') ans = ans + mpp['L']- mpp['X'];
-                else if(s[i+1]== 'C') ans = ans + mpp['C']- mpp['X'];
-                i += 1;
-            }
-                
-            else if(i<n-1 && s[i]== 'C' && (s[i+1]=='D' || s[i+1]=='M')){
-                if(s[i+1]== 'D') ans = ans + mpp['D']- mpp['C'];
-                else if(s[i+1]== 'M') ans = ans + mpp['M']- mpp['C'];
-                i += 1;
-            }
-            
-            else{
-                cout<<s[i]<<" ";
-                ans = ans + mpp[s[i]];
-            }
-                
+        int sum = mpp[s[n-1]];
+        
+        //traversing from the back to front using the map
+        for(int i= n-2; i>=0; i--){
+            if(mpp[s[i]] < mpp[s[i+1]])     //left < right: IV
+                sum -= mpp[s[i]];   //5-1 = 4
+            else
+                sum += mpp[s[i]];
         }
-    
-        return ans;
+        
+        return sum;
     }
 };
