055d03ce9612882e1cc874219a899c24cc042458
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-02 14:24:42 +0530

    Time: 481 ms (30.41%), Space: 287 MB (13.34%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,30 +1,35 @@
-//check every even and odd length substring
+//using dp: use previosuly computed palindromes to compute new one
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ans = "";
         int n = s.size();
+        vector<vector<int>> dp(n, vector<int> (n, 0));
         
+        //fill 1 size & 2 size substring
         for(int i=0; i<n; i++){
-            //even
-            int start = i;
-            int end = i+1;
-            while(start>=0 && end<n && s[start] == s[end]){
-                int len = end- start+ 1;
-                if(len > ans.size())
-                    ans = s.substr(start, len);
-                start--; end++;
+            dp[i][i] = 1;   //single letter is palindrome
+            if(i!= n-1 && s[i]==s[i+1])
+                dp[i][i+1] = 1;
+        }
+        
+        //remaining entries 2>= size, check first and last directly and middle subsstr with dp[][]
+        for(int i=n-3; i>=0; i--){
+            for(int j=i+2; j<n; j++){
+                if(s[i]==s[j] && dp[i+1][j-1]==1)
+                    dp[i][j] =1;
+
             }
-            
-            //odd
-            start = i;
-            end = i;
-            while(start>=0 && end<n && s[start] == s[end]){
-                int len = end-start+1;
-                if(len > ans.size())
-                    ans = s.substr(start, len);
-                
-                start--; end++;
+        }
+        
+        //take out max len palindrome
+        int len = INT_MIN;
+        string ans = "";
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(dp[i][j]==1 && (j-i+1)> len){
+                    len = j-i+1;
+                    ans = s.substr(i, j-i+1);
+                }
             }
         }
         
