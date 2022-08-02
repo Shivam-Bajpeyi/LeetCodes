6146e8c78be94072342c815dd89c2aa078ddcb4e
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-02 12:56:31 +0530

    Time: 22 ms (89.82%), Space: 17.2 MB (47.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,43 +1,33 @@
+//check every even and odd length substring
 class Solution {
 public:
     string longestPalindrome(string s) {
+        string ans = "";
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));   //initialize dp array
         
-        if(n==1)
-            return s;
-        
-        //fill 1 length and 2 length substring values
         for(int i=0; i<n; i++){
-            dp[i][i] = 1; //single letter is palindrome
-            if(i==n-1) break;
+            //even
+            int start = i;
+            int end = i+1;
+            while(start>=0 && end<n && s[start] == s[end]){
+                int len = end- start+ 1;
+                if(len > ans.size())
+                    ans = s.substr(start, len);
+                start--; end++;
+            }
             
-            dp[i][i+1] = (s[i] == s[i+1]); // for ab check if (a== b)
-        }
-        
-        //fill substring which has length >= 3 
-        //for example-> aba (length = 3) string will be palindromic if s[0] == s[2] && nonboundary characters substring is also palindrome(that can be checked by dp[])
-        // for(int i=0; i<n-2; i++){
-        //     for(int j= i+2; j<n; j++){
-        for(int i= n-3; i>=0; --i){
-            for(int j= i+2; j<n; ++j){
-                dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+            //odd
+            start = i;
+            end = i;
+            while(start>=0 && end<n && s[start] == s[end]){
+                int len = end-start+1;
+                if(len > ans.size())
+                    ans = s.substr(start, len);
+                
+                start--; end++;
             }
         }
         
-        //get longest palindromic substring by iterating on dp[]
-        int mx = INT_MIN;   //stores max length
-        string maxStr = ""; //stores longest palindromic substring
-        
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(dp[i][j]==1 && j-i+1>mx){
-                    mx = j-i+1;
-                    maxStr = s.substr(i, j-i+1);
-                }        
-            }
-        }
-        
-        return maxStr;
+        return ans;
     }
 };
