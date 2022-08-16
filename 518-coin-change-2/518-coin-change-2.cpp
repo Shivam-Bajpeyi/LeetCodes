6011b86b536e9c99fdb6fb8bd304278ed76c655e
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-16 10:33:28 +0530

    Time: 53 ms (38.63%), Space: 18.3 MB (48.93%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,30 +1,31 @@
-//top-down approach: to avoid overlapping subproblems
+//Tabulation(bottom-up approach): to avoid overlapping subproblems
 class Solution {
 public:
-    int f(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp){
-        if(ind==0){
-            if(amount%coins[ind]==0)
-                return 1; 
-            else
-                return 0;
-        
-        }
-        
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        
-        //take- not take
-        int notTake = f(ind-1, amount, coins, dp);
-        
-        int take = 0;
-        if(coins[ind]<= amount)
-            take = f(ind, amount-coins[ind], coins, dp);
-        
-        return dp[ind][amount] = notTake+take;
-    }
-    
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return f(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));   //dp[ind][amount]
+        
+        //construct base of the table
+        // for(int i=0; i<n; i++)  //amount= 0
+        //     dp[i][0]= 1;    
+        for(int T=0; T<=amount; T++)    //index = 0
+            dp[0][T] = (T%coins[0]==0);
+        
+        
+        //build dp from bottom to up
+        for(int i=1; i<n; i++){
+            for(int T=0; T<= amount; T++){
+                //take- not take
+                int notTake = dp[i-1][T];
+        
+                int take = 0;
+                if(coins[i]<= T)
+                    take = dp[i][T-coins[i]];
+                
+                dp[i][T] = notTake+take;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
