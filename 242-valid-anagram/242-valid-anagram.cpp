624cae87587a574411f945015d1fa4604a99604c
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-18 01:30:34 +0530

    Time: 17 ms (44.62%), Space: 7.3 MB (46.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size())
-            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
         
-        //t will be anagram of s if every element of s present in t
-        //store  frequency of all char of t in hashmap
-        unordered_map<char, int> freq;
-        for(int i=0; i<t.size(); i++){
-            freq[t[i]]++;
-        }
-        
-        //iterate over s to check every element of s whether present in t or not
-        for(int i=0; i<s.size(); i++){
-            if(freq[s[i]] >=1)
-                freq[s[i]]--;
-            else
-                return false;
-        }
-        
-        return true;
+        cout<<s<<" "<<t<<endl;
+        return (s==t);
     }
 };
