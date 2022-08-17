1efb7cfce38898b65445ce1a19c112b1d089afe6
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-18 01:40:15 +0530

    Time: 11 ms (69.56%), Space: 7.4 MB (7.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if(s.size() != t.size()) return false;
+        int n = s.size();   //n= s.size() = t.size()
+        //if any element occuring in s increase the freq and decrease the frequency for element that occurs in t
+        unordered_map<char, int> mpp;
+        for(int i=0; i<n; i++){
+            mpp[s[i]]++;
+            mpp[t[i]]--;
+        }
         
-        cout<<s<<" "<<t<<endl;
-        return (s==t);
+        for(auto &it: mpp){
+            if(it.second>0) //that means some letter is not occuring equal number of times in both strings
+                return false;
+        }
+        
+        return true;
+           
     }
 };
