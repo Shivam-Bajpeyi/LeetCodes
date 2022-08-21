1bb5cee7acf0fb3645f6847a364414f309c332c8
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-22 02:22:26 +0530

    Time: 13 ms (48.05%), Space: 14.9 MB (42.65%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,43 +11,43 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1)
-            return list2;
-        if(!list2)
-            return list1;
         
-        //In- place
-        //decide starting node: whiever is lesser
-        ListNode* curr = new ListNode(0, NULL);
-        
-        if(list1->val <= list2->val){
+        //decide 1st node
+        ListNode* curr = NULL;
+        if(list1 && list2 && list1->val <= list2->val){
             curr = list1;
             list1 = list1->next;
         }
-        else{
+        else if(list1 && list2 && list1->val > list2->val){
             curr = list2;
             list2 = list2->next;
         }
-        ListNode* head = curr;  //to return head
         
-        //start merging (merge sort)
+        ListNode* ans = curr;
+        //merge sort: take the next small value node and merge to the curr node
         while(list1 && list2){
-            if(list1->val <= list2->val){
+            if(list1->val<= list2->val){
+                // cout<<list1->val<<endl;
                 curr->next = list1;
-                list1 = list1->next;
                 curr = curr->next;
+                list1 = list1->next;
             }
             else{
+                // cout<<list2->val<<endl;
                 curr->next = list2;
                 curr = curr->next;
                 list2 = list2->next;
             }
         }
-        if(list1)
-            curr->next = list1;
-        else
-            curr->next = list2;
         
-        return head;
+        //point next to the list which has node left to add
+        if(list1 || list2){
+            if(curr) 
+                curr->next = (list1 != NULL) ? list1: list2;
+            else 
+                return list1!=NULL? list1: list2;
+        }
+        
+        return ans;
     }
 };
