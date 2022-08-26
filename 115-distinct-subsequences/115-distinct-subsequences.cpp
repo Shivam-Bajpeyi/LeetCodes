d8be8d9e20d475d1454f4fd880b822417b31156e
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-26 18:33:12 +0530

    Time: 54 ms (60.93%), Space: 30.2 MB (7.12%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -26,30 +26,64 @@
 
 
 //DP memoization: use dp array to avoid overlapping subproblems
+// class Solution {
+// public:
+//     int f(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+//         if(j<0)         //we have matched all char in t
+//             return 1;
+//         if(i<0)         //there is nothing to match with now in s
+//             return 0;
+        
+//         if(dp[i][j] != -1) return dp[i][j];
+        
+//         //i matches with j: 2 possibilities whether to take i or not
+//         if(s[i]==t[j])
+//             return dp[i][j] = f(i-1, j-1, s, t, dp) + f(i-1, j, s, t, dp);
+        
+//         //if its not matching, you definitely have to search inside in s1 for jth char 
+//         else
+//             return dp[i][j] = f(i-1, j, s, t, dp);
+//     }
+    
+//     int numDistinct(string s, string t) {
+//         int n = s.size();
+//         int m = t.size();
+        
+//         vector<vector<int>> dp(n, vector<int> (m, -1));
+//         return f(n-1, m-1, s, t, dp);
+//     }
+// };
+
+
+//DP tabulation: make the dp table from bottom to up
 class Solution {
 public:
-    int f(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        if(j<0)         //we have matched all char in t
-            return 1;
-        if(i<0)         //there is nothing to match with now in s
-            return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        //i matches with j: 2 possibilities whether to take i or not
-        if(s[i]==t[j])
-            return dp[i][j] = f(i-1, j-1, s, t, dp) + f(i-1, j, s, t, dp);
-        
-        //if its not matching, you definitely have to search inside in s1 for jth char 
-        else
-            return dp[i][j] = f(i-1, j, s, t, dp);
-    }
-    
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
         
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return f(n-1, m-1, s, t, dp);
+        //shifting of index to right to utilize the base case to make the table
+        vector<vector<double>> dp(n+1, vector<double> (m+1, 0));
+        
+        //use base cases to build the base of the table: -1 -> 0
+        for(int j=0; j<=m; j++) //when s exhauseted; nothing to match with t str
+            dp[0][j] = 0;
+        for(int i=0; i<=n; i++)  //when t exhausted; everyything matched
+            dp[i][0] = 1;
+
+        //form table i= 1 to n & j= 1 to m
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                //i matches with j: 2 possibilities whether to take i or not
+                if(s[i-1]==t[j-1])
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+        
+                //if its not matching, you definitely have to search inside in s1 for jth char      
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return (int)dp[n][m];
     }
 };
