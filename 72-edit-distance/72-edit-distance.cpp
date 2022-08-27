1a3841af722c146f085a541fdbc900bcdbf13721
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-27 16:55:20 +0530

    Time: 21 ms (54.02%), Space: 6.5 MB (94.63%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -48,31 +48,63 @@
 
 
 //3. Tabulation: make a dp table using base cases[right-shift of index] and recurrence
+// class Solution {
+// public:
+//     int minDistance(string word1, string word2) {
+//         int n = word1.size();
+//         int m = word2.size();
+//         //right shift of index -1 -> 0
+//         vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        
+//         //base case
+//         for(int i=0; i<=n; i++) dp[i][0] = i;   //delete i+1 characters to make s1 -> s2
+//         for(int j=0; j<=m; j++) dp[0][j] = j;   //insert j+1 characters to make s1 -> s2
+        
+//         //form table using same recurrence relation used in memoization
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=m; j++){
+//                 //matching- not matching
+//                 if(word1[i-1] == word2[j-1]) 
+//                     dp[i][j] = 0 + dp[i-1][j-1];
+                
+//                 else
+//                     dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1])); 
+//                     //whatever gives min among insert, delete, and replace
+//             }
+//         }
+        
+//         return dp[n][m];
+//     }
+// };
+
+
+//4. Tabulation(space- optimized): using prev array to handle i-1 states
 class Solution {
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
         //right shift of index -1 -> 0
-        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        vector<int> prev(m+1, 0), curr(m+1, 0);
         
         //base case
-        for(int i=0; i<=n; i++) dp[i][0] = i;   //delete i+1 characters to make s1 -> s2
-        for(int j=0; j<=m; j++) dp[0][j] = j;   //insert j+1 characters to make s1 -> s2
+        for(int j=0; j<=m; j++) prev[j] = curr[j] = j;   //insert j+1 characters to make s1 -> s2
         
         //form table using same recurrence relation used in memoization
         for(int i=1; i<=n; i++){
+            curr[0] = i;
             for(int j=1; j<=m; j++){
                 //matching- not matching
                 if(word1[i-1] == word2[j-1]) 
-                    dp[i][j] = 0 + dp[i-1][j-1];
+                    curr[j] = 0 + prev[j-1];
                 
                 else
-                    dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1])); 
+                    curr[j] = 1 + min(curr[j-1], min(prev[j], prev[j-1])); 
                     //whatever gives min among insert, delete, and replace
             }
+            prev = curr;
         }
         
-        return dp[n][m];
+        return curr[m];
     }
 };
