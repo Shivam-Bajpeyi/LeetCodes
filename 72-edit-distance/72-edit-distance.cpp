b73b14f053748de21545a9f78fd69c48b06516f2
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-27 16:44:09 +0530

    Time: 30 ms (35.35%), Space: 9.1 MB (39.74%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -22,26 +22,57 @@
 
 
 //2. DP memoization: to avoid solving overlapping sub-problems
+// class Solution {
+// public:
+//     int f(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
+//         if(i<0) return j+1; //insert j+1 characters to make s1 -> s2
+//         if(j<0) return i+1; //delete i+1 characters to make s1 -> s2
+        
+//         if(dp[i][j] != -1) return dp[i][j];
+        
+//         //matching- not matching
+//         if(word1[i] == word2[j]) return dp[i][j] = 0 + f(i-1, j-1, word1, word2, dp);
+//         else
+//             return dp[i][j] = 1 + min(f(i, j-1, word1, word2, dp), min(f(i-1, j, word1, word2, dp), f(i-1, j-1, word1, word2, dp)));
+//             //whatever gives min among insert, delete, and replace
+//     }
+    
+//     int minDistance(string word1, string word2) {
+//         int n = word1.size();
+//         int m = word2.size();
+//         vector<vector<int>> dp(n, vector<int> (m, -1));
+        
+//         return f(n-1, m-1, word1, word2, dp);
+//     }
+// };
+
+
+//3. Tabulation: make a dp table using base cases[right-shift of index] and recurrence
 class Solution {
 public:
-    int f(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
-        if(i<0) return j+1; //insert j+1 characters to make s1 -> s2
-        if(j<0) return i+1; //delete i+1 characters to make s1 -> s2
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        //matching- not matching
-        if(word1[i] == word2[j]) return dp[i][j] = 0 + f(i-1, j-1, word1, word2, dp);
-        else
-            return dp[i][j] = 1 + min(f(i, j-1, word1, word2, dp), min(f(i-1, j, word1, word2, dp), f(i-1, j-1, word1, word2, dp)));
-            //whatever gives min among insert, delete, and replace
-    }
-    
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
+        //right shift of index -1 -> 0
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
         
-        return f(n-1, m-1, word1, word2, dp);
+        //base case
+        for(int i=0; i<=n; i++) dp[i][0] = i;   //delete i+1 characters to make s1 -> s2
+        for(int j=0; j<=m; j++) dp[0][j] = j;   //insert j+1 characters to make s1 -> s2
+        
+        //form table using same recurrence relation used in memoization
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                //matching- not matching
+                if(word1[i-1] == word2[j-1]) 
+                    dp[i][j] = 0 + dp[i-1][j-1];
+                
+                else
+                    dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1])); 
+                    //whatever gives min among insert, delete, and replace
+            }
+        }
+        
+        return dp[n][m];
     }
 };
