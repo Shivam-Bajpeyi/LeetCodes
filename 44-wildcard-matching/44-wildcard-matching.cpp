71c0d5985012a919216393b4e26f19da7e6bad32
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-29 11:55:40 +0530

    Time: 43 ms (89.38%), Space: 26.9 MB (44.48%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -0,0 +1,73 @@
+//considering s => contains lowercase + '?' + '*'
+//            p => contains only lowercase
+
+
+//1. recursion
+// class Solution {
+// public:
+//     bool f(int i, int j, string &s, string &p){
+//         if(i<0 && j<0) return true;     //both have matched
+//         if(i<0 && j>=0) return false;   //not gonna match
+//         if(j<0 && i>=0){   //if s2 is exhausted, s1 should have only '*'
+//             for(int ii=0; ii<=i; ii++){
+//                 if(s[ii] != '*') return false;
+//             }
+//             return true;
+//         }
+        
+//         //match- '?' will match to the jth char
+//         if(s[i] == p[j] || s[i]=='?') return f(i-1, j-1, s, p);
+        
+//         //if s[i] is astric, try matching by assuming '*' can match with any sequemce in str2 0 to remaining part
+//         if(s[i]== '*')
+//             return f(i-1, j, s, p) || f(i, j-1, s, p);
+//             //assign * nothing || assume it will match with j-1 
+        
+//         //other than above conditions
+//         return false;
+//     }
+    
+//     bool isMatch(string p, string s) {
+//         int n = s.size();
+//         int m = p.size();   //contains lowercase + '?' + '*'
+        
+        
+//         return f(n-1, m-1, s, p);
+//     }
+// };
+
+//2. DP memoization: avoid solving already solved sub-problems
+class Solution {
+public:
+    bool f(int i, int j, string &s, string &p, vector<vector<int>> &dp){
+        if(i<0 && j<0) return true;     //both have matched
+        if(i<0 && j>=0) return false;   //not gonna match
+        if(j<0 && i>=0){   //if s2 is exhausted, s1 should have only '*'
+            for(int ii=0; ii<=i; ii++){
+                if(s[ii] != '*') return false;
+            }
+            return true;
+        }
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        //match + '?' will match to the jth char
+        if(s[i] == p[j] || s[i]=='?') 
+            return dp[i][j] = f(i-1, j-1, s, p, dp);
+        
+        //if s[i] is astric, try matching by assuming '*' can match with any sequemce in str2 0 to remaining part: assign * nothing || assume it will match with j-1 
+        if(s[i]== '*')
+            return dp[i][j] = f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp);
+        
+        //other than above conditions
+        return false;
+    }
+    
+    bool isMatch(string p, string s) {
+        int n = s.size();
+        int m = p.size();   //contains lowercase + '?' + '*'
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(n-1, m-1, s, p, dp);
+    }
+};
