8a6858c668df57154565734059f7ad530ac08a77
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-30 13:59:45 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,19 +1,24 @@
+//Transpose + reverse
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+        int n =matrix.size();
         
-        vector<vector<int>> rotated(n, vector<int>(n));
-        
-        int k = n-1;    //last col
-        //take row and place it from the last to 1st col
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                rotated[j][k] = matrix[i][j];        
+        //tranpose
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i; j<n; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
             }
-            k--;
         }
         
-        matrix = rotated;
+        //rotate
+        for(int i=0; i<n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+            
+       
     }
 };
