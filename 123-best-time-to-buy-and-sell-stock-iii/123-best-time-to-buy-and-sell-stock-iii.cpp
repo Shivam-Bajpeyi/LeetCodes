9a07ca4c86e5a1be29e3ade412f41e866fffaaf4
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-08-31 10:45:00 +0530

    Time: 764 ms (39.98%), Space: 207.1 MB (35.79%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -21,24 +21,52 @@
 
 //2. DP memoization: dp[n][buy][cap]= dp[n][2][3]
 //buy = 2= 0/1, cap = 3= 0/1/2
+// class Solution {
+// public:
+//     int f(int ind, int buy, vector<int> &prices, int cap, vector<vector<vector<int>>> &dp){
+//         if(ind==prices.size()) return 0;
+//         if(cap==0) return 0;
+        
+//         if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+        
+//         if(buy==1)
+//             return dp[ind][buy][cap] = max(-prices[ind]+ f(ind+1, 0, prices, cap, dp), 0+ f(ind+1, 1, prices, cap, dp));
+//         else
+//             return dp[ind][buy][cap] = max(prices[ind]+ f(ind+1, 1, prices, cap-1, dp), 0+ f(ind+1, 0, prices, cap, dp));
+//     }
+    
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        
+//         return f(0, 1, prices, 2, dp);
+//     }
+// };
+
+
+//3. Tabulation: make dp[n][buy][cap] in reverse way 
+// ind = n-1 to 0, buy = 1 to 0, cap= 2 to 0
 class Solution {
 public:
-    int f(int ind, int buy, vector<int> &prices, int cap, vector<vector<vector<int>>> &dp){
-        if(ind==prices.size()) return 0;
-        if(cap==0) return 0;
-        
-        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
-        
-        if(buy==1)
-            return dp[ind][buy][cap] = max(-prices[ind]+ f(ind+1, 0, prices, cap, dp), 0+ f(ind+1, 1, prices, cap, dp));
-        else
-            return dp[ind][buy][cap] = max(prices[ind]+ f(ind+1, 1, prices, cap-1, dp), 0+ f(ind+1, 0, prices, cap, dp));
-    }
-    
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
         
-        return f(0, 1, prices, 2, dp);
+        //ind==0 || cap==0 => dp[ind][buy][cap]= 0;    
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+    
+        //use recurrence relation to make table
+        for(int ind= n-1; ind>=0; ind--){
+            for(int buy=1; buy>=0; buy--){
+                for(int cap=2; cap>=1; cap--){
+                    if(buy==1)
+                        dp[ind][buy][cap] = max(-prices[ind]+ dp[ind+1][0][cap], 0+ dp[ind+1][1][cap]);
+                    else
+                        dp[ind][buy][cap] = max(prices[ind]+ dp[ind+1][1][cap-1], 0+ dp[ind+1][0][cap]); 
+                }
+            }
+        }
+        
+        //ind==0, buy= 0, cap= 0
+        return dp[0][1][2];
     }
 };
