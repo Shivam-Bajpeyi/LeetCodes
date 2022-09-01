d977440c2adeaed363dc873b9ea885ba419d2a6f
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-02 00:19:55 +0530

    Time: 21 ms (8.42%), Space: 12.9 MB (71.47%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -7,58 +7,73 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//BFS + hashmap
 
-//BFS + hashmap(to track parent node)
 class Solution {
 public:
-    void makePar(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &par){
-        if(root==NULL) return;
-        
-        if(root->left){
-            par[root->left]  = root;
-            makePar(root->left, par);
-        }
-        
-        if(root->right){
-            par[root->right] = root;
-            makePar(root->right, par);
+    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track) {
+        queue<TreeNode*> queue;
+        queue.push(root);
+        while(!queue.empty()) { 
+            TreeNode* current = queue.front(); 
+            queue.pop();
+            if(current->left) {
+                parent_track[current->left] = current;
+                queue.push(current->left);
+            }
+            if(current->right) {
+                parent_track[current->right] = current;
+                queue.push(current->right);
+            }
         }
     }
-    
-    //go in all three directions and take nodes with k=0
-    //mark  -1 to the visied nodes
-    void takeNodes(TreeNode* root, int k, unordered_map<TreeNode*, TreeNode*> &par, vector<int> &res){
-        if(root==NULL || k<0) return;
-        if(k==0){
-            res.push_back(root->val);
-            return;
-        }
         
-        root->val = -1;     //visited, no need to check in this direction
-        
-        if(k==0){
-            res.push_back(root->val);
-            return;
-        }
-        
-        //explore in all three directions that had not been explored previosuly
-        if(root->left && root->left->val!= -1)
-            takeNodes(root->left, k-1, par, res);
-        if(root->right && root->right->val!= -1)
-            takeNodes(root->right, k-1, par, res);
-        if(par[root] && par[root]->val!= -1)
-            takeNodes(par[root], k-1, par, res);
-    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        //node-> parent
-        unordered_map<TreeNode*, TreeNode*> par;
-        par[root] = NULL;
-        makePar(root, par);
+        unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
+        markParents(root, parent_track); 
         
-        //take the nodes that have distance k from the target node
-        vector<int> res;
-        takeNodes(target, k, par, res);
+        //2nd BFS to find all the nodes at k dist.
+        int dist = 0;
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*> queue;
+        visited[target] = true;
+        queue.push(target);
+        while(true){
+            if(dist++ == k)
+                break;
+            
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                TreeNode* node = queue.front(); 
+                queue.pop();
+                
+                //upper
+                if(parent_track[node] && !visited[parent_track[node]]){
+                    queue.push(parent_track[node]);
+                    visited[parent_track[node]] = true;
+                }
+                    
+                if(node->left && !visited[node->left]){
+                    queue.push(node->left);
+                    visited[node->left] = true;
+                }
+                    
+                if(node->right && !visited[node->right]){
+                    queue.push(node->right);
+                    visited[node->right] = true;
+                }       
+            }  
+        }
+     
         
-        return res;
+        //put back nodes at dist. k from queue to vector
+        vector<int> ans;
+        int size = queue.size();
+        while(!queue.empty()){
+            ans.push_back(queue.front()->val);
+            queue.pop();
+        }
+        
+        return ans; 
     }
 };
