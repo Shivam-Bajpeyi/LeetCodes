e37c16abbd1f5fc6116c124cd74945d5c954772f
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-01 10:38:01 +0530

    Time: 13 ms (71.25%), Space: 10.9 MB (78.01%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -45,25 +45,54 @@
 
 
 //3. tabulation: dp[n+1][2][k+1]
+// class Solution {
+// public: 
+//     int maxProfit(int k, vector<int>& prices) {
+//         int n = prices.size();
+//         //dp[n+1][2][k+1]
+//         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        
+//         for(int ind= n-1; ind>=0; ind--){
+//             for(int buy=0; buy<=1; buy++){
+//                 for(int cap=1; cap<=k; cap++){
+//                     if(buy==1)
+//                         dp[ind][buy][cap] = max(-prices[ind]+dp[ind+1][0][cap], 0+dp[ind+1][1][cap]);
+                    
+//                     else
+//                         dp[ind][buy][cap] = max(prices[ind]+dp[ind+1][1][cap-1], 0+dp[ind+1][0][cap]);
+//                 }
+//             }
+//         }
+        
+//         return dp[0][1][k];
+//     }
+// };
+
+
+//4. tabulation(space- optimized): using ahead[2][k+1] and curr[2][k+1]
 class Solution {
 public: 
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        //dp[n+1][2][k+1]
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
         
+        //ahead will store ind+1 states
+        vector<vector<int>> ahead(2, vector<int>(k+1, 0)), curr(2, vector<int>(k+1, 0));
+        
+        //put curr as dp[ind] and ahead as dp[ind+1]
         for(int ind= n-1; ind>=0; ind--){
             for(int buy=0; buy<=1; buy++){
                 for(int cap=1; cap<=k; cap++){
                     if(buy==1)
-                        dp[ind][buy][cap] = max(-prices[ind]+dp[ind+1][0][cap], 0+dp[ind+1][1][cap]);
+                        curr[buy][cap] = max(-prices[ind]+ahead[0][cap], 0+ahead[1][cap]);
                     
                     else
-                        dp[ind][buy][cap] = max(prices[ind]+dp[ind+1][1][cap-1], 0+dp[ind+1][0][cap]);
+                        curr[buy][cap] = max(prices[ind]+ahead[1][cap-1], 0+ahead[0][cap]);
                 }
             }
+            
+            ahead = curr;
         }
         
-        return dp[0][1][k];
+        return curr[1][k];
     }
 };
