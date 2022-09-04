817e6f0f89b81cc850ff5d420502e1cca841199f
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-04 22:05:45 +0530

    Time: 2718 ms (5.01%), Space: 292.3 MB (8.58%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,54 @@
+//pick- notPick
+
+//1. recursion
+// class Solution {
+// public:
+//     int f(int ind, int prev, vector<int>& nums){
+//         if(ind== nums.size())
+//             return 0;
+        
+//         //notPick- pick
+//         int notPick = 0+ f(ind+1, prev, nums);
+        
+//         int pick = 0;
+//         if(prev==-1 || nums[ind]> nums[prev])
+//             pick = 1+ f(ind+1, ind, nums);
+        
+//         //max out of it 
+//         return max(notPick, pick);
+//     }
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         return f(0, -1, nums);    //f(ind, prev_ind)
+//     }
+// };
+
+
+//2. DP memoization: dp[n][n]
 class Solution {
 public:
+    int f(int ind, int prev, vector<int>& nums, vector<vector<int>> &dp){
+        if(ind== nums.size())
+            return 0;
+        
+        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
+        
+        //notPick- pick
+        int notPick = 0+ f(ind+1, prev, nums, dp);
+        
+        int pick = 0;
+        if(prev==-1 || nums[ind]> nums[prev])
+            pick = 1+ f(ind+1, ind, nums, dp);
+        
+        //max out of it 
+        return dp[ind][prev+1] = max(notPick, pick);
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
-        int len = INT_MIN;
         int n = nums.size();
-        vector<int> LIS(n, 1);
+        //dp[n][n+1]: 1- based index to deal with prev = -1;
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[i]> nums[j] && LIS[i]==LIS[j])
-                    LIS[i] = 1+LIS[j];
-            }
-            
-            len = max(len, LIS[i]);
-        }
-        
-        return len;
+        return f(0, -1, nums, dp);    //f(ind, prev_ind)
     }
 };
