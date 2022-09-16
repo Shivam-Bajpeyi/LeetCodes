488e3ea1afecaa4c02af69c6a59b51ff3f813c21
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-16 17:36:49 +0530

    Time: 113 ms (55.32%), Space: 10.4 MB (39.58%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -24,32 +24,64 @@
 // };
 
 
-//2. Memoization
+// //2. Memoization
+// class Solution {
+// public:
+//     int f(int i, int j, vector<int>& cuts, vector<vector<int>> &dp){
+//         if(j<i) return 0;
+        
+//         if(dp[i][j] != -1) return dp[i][j];
+        
+//         int mini = INT_MAX;
+//         //cuts in (i, j) blobk
+//         for(int ind = i; ind<=j; ind++){
+//             int value = cuts[j+1] - cuts[i-1] + f(i, ind-1, cuts, dp) + f(ind+1, j, cuts, dp);
+//             mini = min(mini, value);
+//         }
+        
+//         return dp[i][j]= mini;
+//     }
+    
+//     int minCost(int n, vector<int>& cuts) {
+//         int m = cuts.size();
+//         cuts.push_back(n);
+//         cuts.insert(cuts.begin(), 0);
+//         sort(cuts.begin(), cuts.end());
+        
+//         //dp
+//         vector<vector<int>> dp(m+1, vector<int>(m+1, -1));
+//         return f(1, cuts.size(), cuts, dp);
+//     }
+// };
+
+
+//3. Tabulation
 class Solution {
 public:
-    int f(int i, int j, vector<int>& cuts, vector<vector<int>> &dp){
-        if(j<i) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int mini = INT_MAX;
-        //cuts in (i, j) blobk
-        for(int ind = i; ind<=j; ind++){
-            int value = cuts[j+1] - cuts[i-1] + f(i, ind-1, cuts, dp) + f(ind+1, j, cuts, dp);
-            mini = min(mini, value);
-        }
-        
-        return dp[i][j]= mini;
-    }
-    
     int minCost(int n, vector<int>& cuts) {
-        int m = cuts.size();
         cuts.push_back(n);
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
         
+        int m = cuts.size();
         //dp
-        vector<vector<int>> dp(m+1, vector<int>(m+1, -1));
-        return f(1, cuts.size()-2, cuts, dp);
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+        
+        //bottom-up
+        for(int i=m-2; i>=1; i--){
+            for(int j=1; j<=m-2; j++){
+                if(j<i) continue;
+                
+                int mini = INT_MAX;
+                //cuts in (i, j) blobk
+                for(int ind = i; ind<=j; ind++){
+                    int value = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    mini = min(mini, value);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        
+        return dp[1][cuts.size()-2];
     }
 };
