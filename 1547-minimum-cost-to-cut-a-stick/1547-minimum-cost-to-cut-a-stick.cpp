86cb6031b2608badefeeb21563cc0ac10c91fee6
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-16 15:42:44 +0530

    Time: 202 ms (15.26%), Space: 10.2 MB (77.64%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,55 @@
+// 1.recursion
+// class Solution {
+// public:
+//     int f(int i, int j, vector<int>& cuts){
+//         if(j<i) return 0;
+        
+//         int mini = INT_MAX;
+//         //cuts in (i, j) blobk
+//         for(int ind = i; ind<=j; ind++){
+//             int value = cuts[j+1] - cuts[i-1] + f(i, ind-1, cuts) + f(ind+1, j, cuts);
+//             mini = min(mini, value);
+//         }
+        
+//         return mini;
+//     }
+    
+//     int minCost(int n, vector<int>& cuts) {
+//         cuts.push_back(n);
+//         cuts.insert(cuts.begin(), 0);
+//         sort(cuts.begin(), cuts.end());
+        
+//         return f(1, cuts.size()-2,  cuts);
+//     }
+// };
+
+
+//2. Memoization
+class Solution {
+public:
+    int f(int i, int j, vector<int>& cuts, vector<vector<int>> &dp){
+        if(j<i) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int mini = INT_MAX;
+        //cuts in (i, j) blobk
+        for(int ind = i; ind<=j; ind++){
+            int value = cuts[j+1] - cuts[i-1] + f(i, ind-1, cuts, dp) + f(ind+1, j, cuts, dp);
+            mini = min(mini, value);
+        }
+        
+        return dp[i][j]= mini;
+    }
+    
+    int minCost(int n, vector<int>& cuts) {
+        int m = cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(), 0);
+        sort(cuts.begin(), cuts.end());
+        
+        //dp
+        vector<vector<int>> dp(m+1, vector<int>(m+1, -1));
+        return f(1, cuts.size()-2, cuts, dp);
+    }
+};
