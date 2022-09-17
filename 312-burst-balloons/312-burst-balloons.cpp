e9b6cf718abc7fa616a5755f175da3ccccec185a
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-17 12:34:37 +0530

    Time: 702 ms (47.95%), Space: 10.4 MB (52.38%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -25,7 +25,37 @@
 // };
 
 
-//1. Memoization
+//2. Memoization
+// class Solution {
+// public:
+//     int f(int i, int j, vector<int>& nums, vector<vector<int>> &dp){
+//         if(j<i) return 0;       //0 points if there is no balloon
+        
+//         if(dp[i][j] != -1) return dp[i][j];
+        
+//         int maxi = INT_MIN;
+//         for(int ind= i; ind<=j; ind++){
+//             int cost = nums[i-1]*nums[ind]*nums[j+1] + f(i, ind-1, nums, dp) + f(ind+1, j, nums, dp);
+//             maxi = max(maxi, cost);
+//         }
+        
+//         return dp[i][j] = maxi;
+//     }
+    
+//     int maxCoins(vector<int>& nums) {
+//         int n = nums.size();
+//         //dp[n+1][n+1]
+//         vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        
+//         //add both sides 1 for ease of multiplication
+//         nums.insert(nums.begin(), 1);
+//         nums.insert(nums.end(), 1);
+
+//         return f(1, n, nums, dp);
+//     }
+// };
+
+//3. Tabulation
 class Solution {
 public:
     int f(int i, int j, vector<int>& nums, vector<vector<int>> &dp){
@@ -44,13 +74,26 @@ public:
     
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        //dp[n+1][n+1]
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        //dp[n+1][n+1] = 0 to handle base case
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
         
         //add both sides 1 for ease of multiplication
         nums.insert(nums.begin(), 1);
         nums.insert(nums.end(), 1);
 
-        return f(1, n, nums, dp);
+        //i = n to 1 and j= 1 to n
+        for(int i=n; i>=1; i--){
+            for(int j=i; j<=n; j++){
+                int maxi = INT_MIN;
+                for(int ind= i; ind<=j; ind++){
+                    int cost = nums[i-1]*nums[ind]*nums[j+1] + dp[i][ind-1] + dp[ind+1][j];
+                    maxi = max(maxi, cost);
+                }   
+                
+                dp[i][j] = maxi;
+            }
+        }
+        
+        return dp[1][n];
     }
 };
