331e6a3cdf917f3a9d4275ee4f7f99b80c913e2a
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-19 23:19:59 +0530

    Time: 74 ms (17.61%), Space: 32.7 MB (81.79%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -11,37 +11,51 @@
  */
 class Solution {
 public:
-    TreeNode* f(TreeNode* root, int key){
-        if(root==NULL) return root;
-        
-        //return the key node after modification
-        if(root->val ==key){
-            if(!root->left) return root->right;
-            else if(!root->right) return root->left;
-            else{
-                TreeNode* right = root->right;
-                TreeNode* leftmost = right;
+    TreeNode* deleteKey(TreeNode* root, int key){
+        if(!root->left) return root->right;
+        else if(!root->right) return root->left;
+        else{
+            TreeNode* right = root->right;
+            TreeNode* leftmost = right;
                 
-                //go to leftmost node of right
-                while(leftmost->left != NULL)
-                    leftmost = leftmost->left;
+            //go to leftmost node of right
+            while(leftmost->left != NULL)
+                leftmost = leftmost->left;
                 
                 //attach left part to it
                 leftmost->left = root->left;
-                return right;
+                return right;     
+            }
+    }
+
+    TreeNode* deleteNode(TreeNode* node, int key) {
+        if(node== NULL) return node;
+        
+        if(node->val ==key)
+            return deleteKey(node, key);
+        
+        TreeNode* root = node;
+        //find and delete node
+        while(root){
+            if(root->val > key){
+                if(root->left && root->left->val == key){
+                    root->left = deleteKey(root->left, key);
+                    break;
+                } 
+                else
+                    root = root->left;
+            } 
+            
+            else{
+                if(root->right && root->right->val == key){
+                    root->right = deleteKey(root->right, key);
+                    break;
+                }
+                else
+                    root = root->right;
             }
         }
-        
-        if(root->val> key) 
-            root->left = f(root->left, key);
-        else 
-            root->right = f(root->right, key);
-        
-        return root;
-    }
     
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        return f(root, key);
-        // return root;
+        return node;
     }
 };
