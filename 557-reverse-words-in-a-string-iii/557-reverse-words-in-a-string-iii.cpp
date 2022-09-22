d0a7719216c22acca37c9afd95c798e9b84dbb2b
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-22 11:38:55 +0530

    Time: 120 ms (8.49%), Space: 310.3 MB (5.00%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,26 +1,24 @@
+//without reverse function
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans = "";
+        string ans;
+        string word;
         
-        //pick each word and push in ans by reversing it and preserve whitespaces
-        string temp = "";
-        for(int i=0; i<s.size(); i++){
+        //iterate from the back to get the reversed word and place it in the last
+        for(int i=s.size()-1; i>=0; i--){
             if(s[i] != ' ')
-                temp += s[i];
-            else{
-                reverse(temp.begin(), temp.end());
-                ans+= temp;
-                ans+= " ";
-                temp = "";
+                word += s[i];
+            
+            else if(word.size()>=1 && s[i]==' '){
+                ans = word + ans;
+                ans = " "+ ans;         //adding whitespace as it is
+                word = "";          //make it empty
             }
         }
         
-        if(temp.size()>=1){
-            reverse(temp.begin(), temp.end());
-            ans+= temp;
-        }
-            
+        if(word.size()>=1)
+            ans = word + ans;
         
         return ans;
     }
