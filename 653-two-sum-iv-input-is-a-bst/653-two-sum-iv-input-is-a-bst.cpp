8d1c9b19ba215292b1f5010a8d18e9c0ad43dbec
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-26 01:06:28 +0530

    Time: 101 ms (14.52%), Space: 38.9 MB (29.32%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,40 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//set + dfs: tc= O(N), sc= O(N)
 class Solution {
 public:
+    set<int> st;
     bool findTarget(TreeNode* root, int k) {
-        if(root== NULL)
-            return false;
+        if(root==NULL) return false;
         
-        //find inorder(its sorted in itself)
-        vector<int> nums; 
-        inorder(root, nums);
+        if(st.count(k-root->val)) return true;
+        st.insert(root->val);
         
-        //apply 2 sum to find k target in nums[]
-        int start = 0, end = nums.size()-1;
-        while(start< end){
-            int sum = nums[start] + nums[end];
-            if(sum == k)   //k found return true
-                return true;
-            
-            else if(sum< k)
-                start++;
-            else
-                end--;
-        }
-        
-        //if k is not there 
-        return false;
-    }
-    
-    void inorder(TreeNode* root, vector<int> &nums){
-        if(root== NULL)
-            return;
-        
-        //inorder = left root right
-        inorder(root->left, nums);
-        nums.push_back(root->val);
-        inorder(root->right, nums);
+        return findTarget(root->left, k) || findTarget(root->right, k);
     }
 };
