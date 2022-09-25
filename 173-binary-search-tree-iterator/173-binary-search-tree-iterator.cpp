f76abbb9b16740251f70d1bbc45a8cace94a655a
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-26 00:38:25 +0530

    Time: 74 ms (11.02%), Space: 24.1 MB (87.68%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -9,32 +9,67 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//Efficient O(H) space using stack
 class BSTIterator {
 public:
-    vector<int> nums;   //prepare inorder in nums
-    int i=0;            //i will work as iterator
+    stack<TreeNode*> st;
     
-    void inorder(TreeNode* root){     //inorder
-        if(root){
-            inorder(root->left);
-            nums.push_back(root->val);
-            inorder(root->right);
-        }   
+    void storeLeft(TreeNode* root){
+        while(root){
+            st.push(root);
+            root = root->left;
+        }
     }
     
     BSTIterator(TreeNode* root) {
-        inorder(root);
+        storeLeft(root);
     }
     
     int next() {
-       return nums[i++]; 
+        TreeNode* node = st.top();
+        st.pop();
+        int res = node->val; 
+        
+        if(node->right){
+            storeLeft(node->right);
+        }
+        
+        return res;
     }
     
     bool hasNext() {
-        return i<nums.size();
+        return !st.empty();
     }
 };
 
+
+//brute sc= o(N) store inorder somewhere
+// class BSTIterator {
+// public:
+//     vector<int> nums;   //prepare inorder in nums
+//     int i=0;            //i will work as iterator
+    
+//     void inorder(TreeNode* root){     //inorder
+//         if(root){
+//             inorder(root->left);
+//             nums.push_back(root->val);
+//             inorder(root->right);
+//         }   
+//     }
+    
+//     BSTIterator(TreeNode* root) {
+//         inorder(root);
+//     }
+    
+//     int next() {
+//        return nums[i++]; 
+//     }
+    
+//     bool hasNext() {
+//         return i<nums.size();
+//     }
+// };
 /**
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator* obj = new BSTIterator(root);
