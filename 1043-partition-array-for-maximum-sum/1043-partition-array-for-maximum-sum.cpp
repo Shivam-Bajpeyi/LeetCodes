2c323bc6a92d63d3f1a99f41758cf3fbc933fe35
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-28 16:26:09 +0530

    Time: 57 ms (12.15%), Space: 8.7 MB (9.28%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,60 @@
+//1. recursion
+// class Solution {
+// public:
+//     int n;
+    
+//     int f(int i, vector<int> &arr, int k){
+//         if(i==n) return 0;
+         
+//         int sum = INT_MIN;
+//         int mx = INT_MIN;
+//         int len = 0;
+//         //explore: do partition ind = i to i+k
+//         for(int ind=i; ind<min(i+k, n); ind++){
+//             len++;
+//             mx = max(mx, arr[ind]);
+//             int temp =  mx*len + f(ind+1, arr, k);
+//             sum = max(sum, temp);
+//         }
+        
+//         return sum;
+//     }
+    
+//     int maxSumAfterPartitioning(vector<int>& arr, int k) {
+//         n = arr.size();
+        
+//         return f(0, arr, k);
+//     }
+// };
+
+//2. Memoization dp[n]
+class Solution {
+public:
+    int n;
+    
+    int f(int i, vector<int> &arr, int k, vector<int> &dp){
+        if(i==n) return 0;
+         
+        if(dp[i] != -1) return dp[i];
+        
+        int sum = INT_MIN;
+        int mx = INT_MIN;
+        int len = 0;
+        //explore: do partition ind = i to i+k
+        for(int ind=i; ind<min(i+k, n); ind++){
+            len++;
+            mx = max(mx, arr[ind]);
+            int temp =  mx*len + f(ind+1, arr, k, dp);
+            sum = max(sum, temp);
+        }
+        
+        return dp[i] = sum;
+    }
+    
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        n = arr.size();
+        //dp[n] = 0 to n-1
+        vector<int> dp(n, -1);
+        return f(0, arr, k, dp);
+    }
+};
