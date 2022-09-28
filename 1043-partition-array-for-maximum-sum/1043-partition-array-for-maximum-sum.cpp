6707d1d2f6122f1089b37cb383306d66dbae17d5
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-09-28 16:27:47 +0530

    Time: 46 ms (19.59%), Space: 8.6 MB (67.82%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -28,6 +28,38 @@
 // };
 
 //2. Memoization dp[n]
+// class Solution {
+// public:
+//     int n;
+    
+//     int f(int i, vector<int> &arr, int k, vector<int> &dp){
+//         if(i==n) return 0;
+         
+//         if(dp[i] != -1) return dp[i];
+        
+//         int sum = INT_MIN;
+//         int mx = INT_MIN;
+//         int len = 0;
+//         //explore: do partition ind = i to i+k
+//         for(int ind=i; ind<min(i+k, n); ind++){
+//             len++;
+//             mx = max(mx, arr[ind]);
+//             int temp =  mx*len + f(ind+1, arr, k, dp);
+//             sum = max(sum, temp);
+//         }
+        
+//         return dp[i] = sum;
+//     }
+    
+//     int maxSumAfterPartitioning(vector<int>& arr, int k) {
+//         n = arr.size();
+//         //dp[n] = 0 to n-1
+//         vector<int> dp(n, -1);
+//         return f(0, arr, k, dp);
+//     }
+// };
+
+//tabulation dp[n] = build n-1 to 0
 class Solution {
 public:
     int n;
@@ -53,8 +85,25 @@ public:
     
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         n = arr.size();
+        
         //dp[n] = 0 to n-1
-        vector<int> dp(n, -1);
-        return f(0, arr, k, dp);
+        vector<int> dp(n, 0);
+        
+        for(int i=n-1; i>=0; i--){
+            int sum = INT_MIN;
+            int mx = INT_MIN;
+            int len = 0;
+            //explore: do partition ind = i to i+k
+            for(int ind=i; ind<min(i+k, n); ind++){
+                len++;
+                mx = max(mx, arr[ind]);
+                int temp =  mx*len + f(ind+1, arr, k, dp);
+                sum = max(sum, temp);
+            }   
+            
+            dp[i] = sum;
+        }
+        
+        return dp[0];
     }
 };
