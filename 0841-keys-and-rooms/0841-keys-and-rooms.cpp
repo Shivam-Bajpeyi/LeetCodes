bbb69d1feea7c43bb1aa97ac44014c28797f4f2f
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-11-15 00:52:23 +0530

    Time: 23 ms (34.84%), Space: 10.6 MB (33.73%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void bfs(int i, vector<int> &vis, vector<int> adj[]){
+    void bfs(int i, vector<int> &vis, vector<vector<int>>& rooms){
         queue<int> que;
         que.push(i);
         vis[i] = 1;
@@ -12,7 +12,7 @@ public:
                 int node = que.front();
                 que.pop();
                 
-                for(int& it: adj[node]){
+                for(auto& it: rooms[node]){
                     if(vis[it]== -1){            //not visited
                         vis[it] = 1;
                         que.push(it);
@@ -22,16 +22,10 @@ public:
         }
     }
     
+    //do graph traversal(BFS) and use rooms as adj[] list
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<int> adj[n];
-        
-        //prepare adj list
-        for(int i=0; i<n; i++){
-            for(auto& jt: rooms[i]){
-                adj[i].push_back(jt);
-            }
-        }
         
         
         //BFS
@@ -39,7 +33,7 @@ public:
         vis[0] = 1;
         for(auto& it: rooms[0]){        //we have only access to room1
             if(vis[it]==-1)
-                bfs(it, vis, adj);
+                bfs(it, vis, rooms);
         }
         
         //check whether all gates are travelled
