78054e857957258e30f02e1a7bdd8e7323e411ef
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-12-03 12:57:39 +0530

    Time: 269 ms (96.66%), Space: 78.4 MB (89.38%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
         //hashmap to count unique times for an id
-        unordered_map<int, set<int>> mpp1;
+        unordered_map<int, set<int>> mpp;
         
         //0->{5, 2}
         for(auto &it: logs){
-            mpp1[it[0]].insert(it[1]);
-        }
-        
-        //hashmap to count same size sets-> ids that have same number of timings
-        unordered_map<int, int> mpp2;
-        for(auto &it: mpp1){
-            mpp2[it.second.size()]++;    
+            mpp[it[0]].insert(it[1]);
         }
         
         //get the ans
         vector<int> ans(k);
-        for(auto &it: mpp2){
-            ans[it.first-1] = it.second;
+        for(auto &it: mpp){
+            ans[it.second.size()-1]++;
         }
         
         return ans;
