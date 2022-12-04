f80d47b199497da32bb0a94b6e7c66cb494934b2
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-12-05 03:09:54 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -9,21 +9,20 @@ using namespace std;
 
 class Solution {
   public:
-  void dfs(int i, int j, int base1, int base2, vector<pair<int, int>> &vec, vector<vector<int>>& grid){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] != 1)
+  void dfs(int i, int j, int base1, int base2, vector<pair<int, int>> &vec, vector<vector<int>>& grid, 
+  vector<vector<int>> &vis){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] != 1 || vis[i][j] ==1)
             return;
         
-        // int tmp = grid[i][j];
-        grid[i][j] = 2;
-        
+        vis[i][j] = 1;
         //store shape in set
         vec.push_back({i-base1, j-base2});
         
         //explore all 4 dir
-        dfs(i+1, j, base1, base2, vec, grid);      //up
-        dfs(i-1, j, base1, base2, vec, grid);      //down
-        dfs(i, j+1, base1, base2, vec, grid);      //right
-        dfs(i, j-1, base1, base2, vec, grid);      //left
+        dfs(i+1, j, base1, base2, vec, grid, vis);      //up
+        dfs(i-1, j, base1, base2, vec, grid, vis);      //down
+        dfs(i, j+1, base1, base2, vec, grid, vis);      //right
+        dfs(i, j-1, base1, base2, vec, grid, vis);      //left
         
   }
   
@@ -32,14 +31,17 @@ class Solution {
         n = grid.size();
         m = grid[0].size();
         
+        //vis
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+        
         //to store unique islands
         set<vector<pair<int, int>>> st;
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j]==1){
+                if(grid[i][j]==1 && vis[i][j] != 1){
                     vector<pair<int, int>> vec;
-                    dfs(i, j, i, j, vec, grid);
+                    dfs(i, j, i, j, vec, grid, vis);
                     st.insert(vec);
                 }
                     
