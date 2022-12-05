33de3ab2e728eadacd48ef0ec60ec424d2034910
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-12-05 22:55:49 +0530

    Time: 31 ms (81.83%), Space: 13.7 MB (39.59%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,22 +1,34 @@
+//BFS: color with 0 and 1, unvisited with  -1
 class Solution {
 public:
-    bool dfs(int node, int color, vector<vector<int>>& graph, vector<int> &vis){
+    bool bfs(int node, int color, vector<vector<int>>& graph, vector<int> &vis){
         // cout<<node<<" "<<color<<endl;
+        queue<int> q;
+        q.push(node);
         vis[node] = color;
         
         //go to neighbours
-        for(auto &it: graph[node]){
-            if(vis[it]==-1)         //if not visited, color with opposite color
-            {
-                if(dfs(it, 1-color, graph, vis)==false)
-                    return false;
-            }
-                
+        while(!q.empty()){
+            int sz = q.size();
             
-            //if colored then check
-            else if(vis[it]==color){
-                // cout<<node<<"->"<<color<<" "<<it<<"->"<<vis[it]<<endl;
-                return false;
+            while(sz--){
+                int it = q.front();
+                q.pop();
+                color = vis[it];
+                // cout<<it<<endl;
+                
+                for(auto &jt: graph[it]){
+                    if(vis[jt]==-1){         //if not visited, color with opposite color
+                        vis[jt]= 1-color;
+                        q.push(jt);
+                    }
+                
+                    //if colored then check: 2 adj nodes have same color?
+                    else if(vis[jt]==color){
+                        return false;
+                    }     
+                }
+                
             }
         }
         
@@ -24,11 +36,11 @@ public:
     }
     
     bool isBipartite(vector<vector<int>>& graph) { 
-        // dfs
+        // bfs
         vector<int> vis(graph.size(), -1);
         for(int i=0; i<graph.size(); i++){
             //if two adjacent nodes can't be colored with 2 diff colors
-            if(vis[i]==-1 && dfs(i, 0, graph, vis)==false){
+            if(vis[i]==-1 && bfs(i, 0, graph, vis)==false){
                 return false;
             }
         }
