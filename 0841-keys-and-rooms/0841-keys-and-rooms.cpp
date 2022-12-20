18b65a43b1da26280bb8ca20ef9eb35e1d58bba9
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-12-20 10:41:54 +0530

    Time: 22 ms (38.93%), Space: 10.5 MB (58.80%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,32 +1,26 @@
 class Solution {
-public:
-    void dfs(int i, vector<int> &vis, vector<vector<int>>& rooms){      
-        vis[i] = 1;
+public:  
+    void dfs(int node, vector<int> &vis, vector<vector<int>>& rooms){
+        vis[node] = 1;
         
-        //dfs
-        for(auto& it: rooms[i]){
-            if(vis[it]== -1){            //not visited
+        for(auto &it: rooms[node]){
+            if(vis[it]==-1){
                 dfs(it, vis, rooms);
             }
-        }        
+        }
     }
     
-    //do graph traversal(BFS) and use rooms as adj[] list
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        //do Graph trversal (dfs/bfs) from 0 to see whether all nodes can be visited 
         int n = rooms.size();
+        vector<int> vis(n, -1);
         
         //dfs
-        vector<int> vis(n, -1);
-        vis[0] = 1;
-        for(auto& it: rooms[0]){        //we have only access to room1
-            if(vis[it]==-1)
-                dfs(it, vis, rooms);
-        }
+        dfs(0, vis, rooms);
         
-        //check whether all gates are travelled
-        for(int i=0; i<n; i++)
-        {
-            if(vis[i]==-1)          //any gate is not visited
+        //check whether all node visited?
+        for(auto &it: vis){
+            if(it==-1)
                 return false;
         }
         
