e1704ac692fb29856159cfa563912476d772ff51
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-12-20 10:44:26 +0530

    Time: 26 ms (22.26%), Space: 10.4 MB (58.80%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:  
-    void dfs(int node, vector<int> &vis, vector<vector<int>>& rooms){
-        vis[node] = 1;
-        
-        for(auto &it: rooms[node]){
-            if(vis[it]==-1){
-                dfs(it, vis, rooms);
-            }
-        }
-    }
-    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         //do Graph trversal (dfs/bfs) from 0 to see whether all nodes can be visited 
         int n = rooms.size();
         vector<int> vis(n, -1);
         
-        //dfs
-        dfs(0, vis, rooms);
+        //bfs
+        queue<int> q;
+        q.push(0);
+        vis[0] = 1;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for(auto &it: rooms[node]){
+                if(vis[it]==-1){
+                    vis[it]= 1;
+                    q.push(it);
+                }
+            }
+        }
         
         //check whether all node visited?
         for(auto &it: vis){
