a1b9f7045bc1eb1cdbc47d6b615628fc0998b0e9
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2022-12-30 15:17:22 +0530

    Time: 6 ms (26.70%), Space: 7.7 MB (60.40%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
@@ -1,50 +1,19 @@
 class Solution {
 public:
     int captureForts(vector<int>& forts) {
-        int n = forts.size(), ans = INT_MIN;
+        //we have to find the max number of zeroes between 1 & -1 or -1 & 1
+        int n = forts.size();
         
-        //left to right
+        int j= 0, res= 0;
         for(int i=0; i<n; i++){
-            if(forts[i] != 1) continue;
-            if(i<n-1 && forts[i+1]!= 0)
-                continue;
-            
-            i++;
-            int temp = 0;
-            while(i<n-1 && forts[i]==0){
-                temp++;
-                i++;
-            }      
-            if(i<n && forts[i]== -1){
-                ans = max(ans, temp);
-            }    
-            else
-                i--;
-        }
-        
-        //right to left
-        for(int i=n-1; i>=0; i--){
-            if(forts[i] != 1) continue;
-            if(i>0 && forts[i-1]!= 0)
-                continue;
-            
-            i--;
-            int temp = 0;
-            while(i>0 && forts[i]==0){
-                temp++;
-                i--;
+            if(forts[i] != 0){
+                if(forts[i] == -forts[j])
+                    res = max(res, i-j-1);
+                
+                j = i;
             }
-            // cout<<temp<<" ";
-            if(i>=0 && forts[i]== -1){
-                ans = max(ans, temp);
-                // cout<<ans;
-            }    
-            else
-                i++;
         }
         
-        if(ans == INT_MIN)
-            return 0;
-        return ans;
+        return res;
     }
 };
