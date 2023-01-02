7d0b338098f6207efeff82e02185df9ba06ae4a6
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2023-01-02 17:29:40 +0530

    Time: 4 ms (46.95%), Space: 6.4 MB (36.63%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -7,32 +7,17 @@ public:
             mpp[it]++;
         }
        
-        //ans = make strings with letters with high -> lower priority
-        //ans2 = string whose letters have no priority set
+        //make ans
         string ans = "";
-        string ans2 = "";
-        
-        //ans
         for(auto &it: order){
-            //if the priority letter is present in s then add it 
-            if(mpp.find(it) != mpp.end()){
-                while(mpp[it]>0){
-                    ans += it;
-                    mpp[it]--;
-                }
-            }
+            ans += string(mpp[it], it);
+            mpp[it] = 0;
         }
         
-        //ans2 = collect non-priority letters(freq != 0)
-        for(auto &it: s){
-            if(mpp[it]!=0){
-                while(mpp[it]>0){
-                    ans2 += it;
-                    mpp[it]--;
-                }
-            }
+        for(auto &it: mpp){
+            ans += string(it.second, it.first);
         }
         
-        return ans+ans2;
+        return ans;
     }
 };
