bfca5ac30118198b2fe39d2e3c5d18af9d8eb5fa
shm_47 <62853400+Shivam-Bajpeyi@users.noreply.github.com>
2023-02-13 20:10:49 +0530

    Added solution - LeetHub

---
+++ Strongly Connected Components (Kosaraju's Algo) - GFG/strongly-connected-components-kosarajus-algo.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+	private:
+	//to store nodes acc to finishing time
+	void dfs(int node, vector<int> &vis, vector<int> adj[], stack<int> &st){
+	    vis[node] = 1;
+
+	    for(auto &it: adj[node]){
+	        if(vis[it] == -1){
+	            dfs(it, vis, adj, st);
+	        }
+	    }
+	    st.push(node);
+	}
+	
+	//to explore the scc one by one
+	void dfs2(int node, vector<int> &vis, vector<int> adj[]){
+	    vis[node] = 1;
+
+	    for(auto &it: adj[node]){
+	        if(vis[it] == -1){
+	            dfs2(it, vis, adj);
+	        }
+	    }
+	}
+	
+	public:
+	//Function to find number of strongly connected components in the graph.
+    int kosaraju(int V, vector<int> adj[])
+    {
+        //visited
+        vector<int> vis(V, -1);
+        
+        //step-1: sort nodes in stack acc to finishing time so that scc1 nodes will be
+        //stored at the top in stack
+        stack<int> st;
+        for(int i=0; i<V; i++){
+            if(vis[i] == -1)
+                dfs(i, vis, adj, st);
+        }
+        
+
+        //step-2: reverse the graph
+        vector<int> reverseAdj[V];
+        for(int i=0; i<V; i++){
+            vis[i] = -1;
+            for(auto &it: adj[i]){
+                reverseAdj[it].push_back(i);
+            }
+        }
+        
+        
+        //step-3: DFS to count scc acc to node stored ------>
+        int cnt = 0;
+        while(!st.empty()){
+            int i = st.top();
+            st.pop();
+            
+            if(vis[i] == -1){
+                cnt++;
+                dfs2(i, vis, reverseAdj);
+            }
+        }
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
